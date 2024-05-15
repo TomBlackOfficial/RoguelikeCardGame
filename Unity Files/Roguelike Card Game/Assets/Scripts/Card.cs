e6096ffe8d2dba80739dfd3b533b07ec59f46fc3
Tom Black <tomblackbusiness@gmail.com>
2024-05-15 15:47:42 -0700

    Card scriptable object

--- Unity Files/Roguelike Card Game/Assets/Scripts/Card.cs
+++ Unity Files/Roguelike Card Game/Assets/Scripts/Card.cs
@@ -5,7 +5,30 @@ using TMPro;
 
 public class Card : MonoBehaviour
 {
-    public int currentHealth, attackPower, manaCost;
+    public CardScriptableObject cardSO;
 
-    [SerializeField] private TMP_Text healthText, attackText, costText;
+    public int health { private set; get; }
+    public int attack { private set; get; }
+    public int manaCost { private set; get; }
+
+    [SerializeField] private TMP_Text nameText, descriptionText, healthText, attackText, costText;
+
+    private void Start()
+    {
+        SetupCard();
+    }
+
+    public void SetupCard()
+    {
+        health = cardSO.health;
+        attack = cardSO.attack;
+        manaCost = cardSO.manaCost;
+
+        healthText.text = health.ToString();
+        attackText.text = attack.ToString();
+        costText.text = manaCost.ToString();
+
+        nameText.text = cardSO.cardName;
+        descriptionText.text = cardSO.description;
+    }
 }
---
+++ Unity Files/Roguelike Card Game/Assets/Scripts/CardScriptableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card", order = 1)]
+public class CardScriptableObject : ScriptableObject
+{
+    public string cardName;
+
+    [TextArea]
+    public string description;
+
+    public int health, attack, manaCost;
+}
