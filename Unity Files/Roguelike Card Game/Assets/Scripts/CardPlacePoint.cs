f639edcc49abe6d6831705503e7e0f1da03030d8
DanielWhang885 <DanielWhang885@gmail.com>
2024-06-02 21:18:47 -0700

    Added a camera effect that zooms in when a card is placed. Needs work

--- Unity Files/Roguelike Card Game/Assets/Scripts/CardPlacePoint.cs
+++ Unity Files/Roguelike Card Game/Assets/Scripts/CardPlacePoint.cs
@@ -1,9 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CardPlacePoint : MonoBehaviour
 {
     public Card activeCard;
     public bool isPlayerPoint;
+    
+    //Daniel's code
+    private Card testCard;
+    [SerializeField] private CinemachineVirtualCamera pairedCamera;
+    [SerializeField] private float cameraFocusTime = 3.0f;
+
+    private void Start()
+    {
+        Redo();
+        pairedCamera.Priority = 0;
+    }
+
+    private void Update()
+    {
+        if (activeCard != testCard)
+        {
+            Redo();
+            SwitchCamera();
+        }
+    }
+
+    private void Redo()
+    {
+        testCard = activeCard;
+    }
+    
+    private void SwitchCamera()
+    {
+        pairedCamera.Priority = 20;
+        Invoke("CameraOff", cameraFocusTime); 
+
+    }
+
+    private void CameraOff()
+    {
+        pairedCamera.Priority = 0;
+    }
 }
